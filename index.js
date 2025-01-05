2d50c734f8e8f329521795051c7c92258fb413a9
Juan Carlos Landero <jcarlos210193@gmail.com>
2025-01-05 12:26:36 -0600

    Agrega codigo para conexion con el broker

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const io = require('socket.io')(serverHttp, {
 	}
 });
 const db = require('./models');
+const mqtt = require('mqtt');
+
 const Device = db.Device;
+const DeviceData = db.DeviceData;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +27,9 @@ app.listen(process.env.API_PORT, () => {
 	console.log(`Puerto API ${process.env.API_PORT}`);
 });
 
-// Implementación del websocket
+/**
+ * Implementación del websocket
+ */
 const socket = io.on('connection', (ioSocket) => {
 	console.log(`Se estableció conexión con el websocket, puerto: ${process.env.WEBSOCKET_PORT}`);
 
@@ -46,6 +51,10 @@ const socket = io.on('connection', (ioSocket) => {
 		if (connectedDevice) {
 			// Se ejecuta cuando connectedDevice tiene datos de un dispositivo.
 			console.log(`Conectado al dispositivo: ${connectedDevice.key}.`);
+
+			// El nombre de la "room" para poder enviar los "mensajes" o datos
+			//  hacia un dispositivo especifico
+			socket.join(`dispositivo-${connectedDevice.id}`);
 		} else {
 			// Se ejecuta cuando connectedDevice es igual a null.
 			console.log('No se encontró el dispositivo.');
@@ -56,3 +65,48 @@ const socket = io.on('connection', (ioSocket) => {
 
 // Expone el websocket por el puerto indicado
 serverHttp.listen(process.env.WEBSOCKET_PORT);
+
+
+/**
+ * Implementación del broker
+ */
+const mqttClient = mqtt.connect('http://emqx');
+
+mqttClient.on('connect', () => {
+	console.log('Se conecto a mqtt.');
+});
+
+// topic: /dispositivos/abcdfe
+// El signo + es un comodín
+mqttClient.subscribe('/dispositivos/+');
+
+// topic = /dispositivos/w21qCFJujv
+// message = "{"temperatura": 35}"
+mqttClient.on('message', async (topic, message) => {
+	// Obtenemos la key de la variable topic
+	const deviceKey = topic.split('/')[2];
+
+	console.log(`Dispositivo ${deviceKey} publicando.`);
+	
+	// Se verifica que el dispositivo exista en base de datos
+	const connectedDevice = await Device.findOne({
+		where: {
+			key: deviceKey
+		}
+	});
+
+	if (connectedDevice) {
+		// Se transforman los datos a un objeto JSON valido.
+		const data = JSON.parse(message.toString());
+
+		// "Query" para guardar los datos recibidos en nuestra tabla devices_data.
+		await DeviceData.create({
+			device_id: connectedDevice.id,
+			topic: topic,
+			data: message.toString()
+		});
+
+		socket.in(`dispositivo-${connectedDevice.id}`).emit('temperatura', { date: Date(), value: data.temperatura });
+		socket.in(`dispositivo-${connectedDevice.id}`).emit('luminosidad', { value: data.luminosidad });
+	}
+});
